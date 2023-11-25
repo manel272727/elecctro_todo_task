a78996fb019a6dab179794b556aacb7506afaea3
neco2021 <77984919+neco2021@users.noreply.github.com>
2023-11-25 16:14:24 +0100

    localstorage login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Main from './componentes/Main';
+// import Main from './todo_components/Main';
+import CredentialsCheck from './login_components/credentials_check';
 
 
 const  App = () => {
@@ -8,7 +9,8 @@ const  App = () => {
 
   return (
     <div className='App'>
-     <Main/>
+      <CredentialsCheck/>
+     {/* <Main/> */}
     </div>
   );
 }
