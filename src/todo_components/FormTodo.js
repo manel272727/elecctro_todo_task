0c6783c5034b48e60435f23c0f4ac1816ac3ad8c
neco2021 <77984919+neco2021@users.noreply.github.com>
2023-12-02 20:53:57 +0100

    fetch data

--- src/todo_components/FormTodo.js
+++ src/todo_components/FormTodo.js
@@ -1,3 +1,54 @@
+// import React, { useState } from "react";
+// import Button from 'react-bootstrap/Button';
+// import '../App.css'; 
+
+// const FormTodo = ({ addTodo }) => {
+//   const [value, setValue] = useState("");
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await fetch('http://localhost:3001/api/todos', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ todo_body: value }),
+//       });
+
+//       if (!response.ok) {
+//         throw new Error('Failed to add todo');
+//       }
+
+//       // If successful, add the todo locally to update UI
+//       addTodo(value);
+//       setValue("");
+//     } catch (error) {
+//       console.error('Error adding todo:', error);
+//       // Handle error scenario (show error message, etc.)
+//     }
+//   };
+
+//   return (
+//     <form className="TodoForm" onSubmit={handleSubmit}>
+//       <div className="input-group mb-3">
+//         <input
+//           type="text"
+//           className="form-control"
+//           value={value}
+//           placeholder="Escreve aqui as tuas tarefas"
+//           onChange={(e) => setValue(e.target.value)}
+//           required
+//         />
+//         <Button className="todo-btn"  variant="primary" type="submit">Adicionar</Button>
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default FormTodo;
+
 import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import '../App.css'; 
@@ -5,11 +56,32 @@ import '../App.css';
 const FormTodo = ({ addTodo }) => {
   const [value, setValue] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/todos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ todo_body: value }), // Send the todo_body property
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to add todo');
+      }
 
-    addTodo(value);
-    setValue("");
+      // If successful, add the todo locally to update UI
+      addTodo(value);
+      setValue("");
+    } catch (error) {
+      console.error('Error adding todo:', error);
+      // Handle error scenario (show error message, etc.)
+    }
+  };
+
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    handleSubmit();
   };
 
   return (
@@ -23,9 +95,14 @@ const FormTodo = ({ addTodo }) => {
           onChange={(e) => setValue(e.target.value)}
           required
         />
-        
-        <Button className="todo-btn"  variant="primary" type="submit">Adicionar</Button>
-          
+        <Button
+          className="todo-btn"
+          variant="primary"
+          onClick={handleButtonClick} // Call handleButtonClick when clicked
+          type="submit"
+        >
+          Adicionar
+        </Button>
       </div>
     </form>
   );
--- src/todo_components/Main.js
+++ src/todo_components/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FormTodo from "./FormTodo";
 import { v4 as uuidv4 } from "uuid";
 import Todo from "./Todo";
@@ -15,6 +15,7 @@ uuidv4();
 const Main = () => {
   const [todos, setTodos] = useState([]);
   const [showCompleted, setShowCompleted] = useState(false);
+  const [fetchedTodos, setFetchedTodos] = useState([]);
 
   const addTodo = (todo) => {
     setTodos([
@@ -49,6 +50,35 @@ const Main = () => {
         todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
       )
     );
+  };
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/data');
+      if (!response.ok) {
+        throw new Error(`Failed to fetch todos: ${response.statusText}`);
+      }
+      const contentType = response.headers.get('content-type');
+      if (contentType && contentType.includes('application/json')) {
+        const data = await response.json();
+        setTodos(data);
+        setFetchedTodos(data);
+      } else {
+        const text = await response.text();
+        console.error('Unexpected response format:', text);
+      }
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
+  
+
+  useEffect(() => {
+    fetchTodos(); // Fetch todos when the component mounts
+  }, []);
+
+  const handleFetchTodos = () => {
+    fetchTodos(); // Call the fetch function to retrieve todos when the button is clicked
   };
 
 
@@ -121,6 +151,19 @@ const Main = () => {
             ) : null
           )}
         </TransitionGroup>
+        <Button onClick={handleFetchTodos}>
+              HERE TO FETCH   
+        </Button>
+        <div>
+        <h2>Fetched Todos:</h2>
+        <ul>
+          {fetchedTodos.map(todo => (
+            // <li key={todo.id}>{todo.task}</li>
+            <li key={todo.id}>{todo.todo_body}</li>
+          ))}
+        </ul>
+      </div>
+      
       </div>
     </div>
   );
